5ebc4698701cf4efe13a358da445cfef8891436c
Ercan Karakaya <ercankarakaya@ycp.com.tr>
2021-12-18 15:11:36 +0300

    test commit-3

--- src/main/java/com/ercan/configurations/AuditorConfig.java
+++ src/main/java/com/ercan/configurations/AuditorConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
 /**
  * @author Ercan Karakaya 04.12.2021
  */
